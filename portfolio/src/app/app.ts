d88e43ad5c6334fff309dcda38f275f7ca46163b
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2025-12-21 08:11:08 +0100

    Hero Section mit SCSS Variables, Anta und Josefin Sans Fonts abgeschlossen

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
@@ -10,7 +9,7 @@ import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, About, Skills, Projects, Contact, Footer],
+  imports: [Header, Hero, About, Skills, Projects, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
